cc829b4c3827482959e21ea1b30162af384249bf
Gregory Cooke <gtcooke94@gmail.com>
2020-06-25 16:43:39 -0400

    Add some types

--- project1/src/server.ts
+++ project1/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express'
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -26,7 +27,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     //    image_url: URL of a publicly accessible image
     // RETURNS
     //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-    app.get("/filteredimage", async(req, res) => {
+    app.get("/filteredimage", async(req: Request, res: Response) => {
         let {image_url} = req.query;
         if (!image_url) {
             return res.status(400).send("Must supply an image url")
@@ -50,7 +51,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
     // Root Endpoint
     // Displays a simple message to the user
-    app.get( "/", async ( req, res ) => {
+    app.get( "/", async ( req: Request, res: Response ) => {
         res.send("try GET /filteredimage?image_url={{}}")
     } );
 
